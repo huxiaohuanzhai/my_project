60177b6e912cd22134beb92a152539e5c278caa8
huxiaohuanzhai <69295680+huxiaohuanzhai@users.noreply.github.com>
2023-06-07 21:02:01 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Message from './components/Message';
+import ListGroup from './components/ListGroup';
+import { Fragment } from 'react';
+
+function App() {
+  return <>
+            <Message>
+            </Message>
+            <ListGroup>
+            </ListGroup>
+         </>;
+}
+
+export default App;
